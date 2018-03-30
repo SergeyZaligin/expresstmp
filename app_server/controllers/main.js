fc379f8cf1eb3f62576a26d11b76515117025812
sergey <sergey>
2018-03-30 15:01:20 +0300

    api

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,3 +1,13 @@
-module.exports.index = function(req, res) {
-	res.render('index', { title: "Tmp Express!!!" })
+var request = require('request');
+
+var apiOptions = {
+  server: 'http://localhost:3000'
+};
+
+if (process.env.NODE_ENV === 'production') {
+  apiOptions.server = 'https://my.site.com';
 }
+
+module.exports.index = function(req, res) {
+  res.render('index', { title: 'Tmp Express!!!' });
+};
