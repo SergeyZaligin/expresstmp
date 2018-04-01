f003bded8dca278ac46a40a8d667eb8851fce53b
cobweb <codename.cobweb@gmail.com>
2018-04-01 16:50:50 +0300

    fix

--- app_api/controllers/main.js
+++ app_api/controllers/main.js
@@ -1,11 +1,32 @@
 var Vacation = require('../models/vacation');
 
-var sendJSONresponse = function(res, status, content) {
-  res.status(status);
-  res.json(content);
+var sendJSONresponse = function (res, status, content) {
+    res.status(status);
+    res.json(content);
 };
 
-module.exports.index = function(req, res) {
-  
-  sendJSONresponse(res, 200, { status: 'success' });
+module.exports.index = function (req, res) {
+    sendJSONresponse(res, 200, {
+        status: 'success'
+    });
+};
+
+module.exports.vacations = function (req, res) {
+    Vacation.find({
+        available: true
+    }, function (err, vacations) {
+        var context = {
+            vacations: vacations.map(function (vacation) {
+                return {
+                    sku: vacation.sku,
+                    name: vacation.name,
+                    description: vacation.description,
+                    price: vacation.getDisplayPrice(),
+                    inSeason: vacation.inSeason,
+                }
+            })
+        };
+        console.log(context);
+        res.render('vacations', context);
+    });
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var controllerMain = require('../controllers/main.js');
+
 /* GET home page. */
 router.get('/', controllerMain.index);
+router.get('/vacations', controllerMain.vacations);
 
 module.exports = router;
