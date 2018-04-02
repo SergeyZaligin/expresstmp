170246fb36d1115332049bfe6ad69edb614e6c4f
sergey <sergey>
2018-04-02 12:37:51 +0300

    fix

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,21 +1,24 @@
 var request = require('request');
 
 var apiOptions = {
-    server: 'http://localhost:3000'
+  server: 'http://localhost:3000'
 };
 
 if (process.env.NODE_ENV === 'production') {
-    apiOptions.server = 'https://my.site.com';
+  apiOptions.server = 'https://my.site.com';
 }
 
-module.exports.index = function (req, res) {
-
-    request('http://localhost:3000/api/vacations', function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
-        
-        res.render('index', { body });
+module.exports.index = function(req, res) {
+  request('http://localhost:3000/api/vacations', function(
+    error,
+    response,
+    body
+  ) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    var body1 = JSON.parse(body);
+    console.log(typeof body1);
+    res.render('index', { body1 });
   });
-    
 };
